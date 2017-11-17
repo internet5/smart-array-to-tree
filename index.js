b233f73e233ed5601691dba716e7c8eb3dc74194
internet5 <172039711@qq.com>
2017-11-17 17:55:57 +0800

    init
    init

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var groupBy = require('lodash.groupby')
+const smartArrayToTree = ({ array, id = 'id', pid = 'pid', children = 'children', firstPid = null }) => {
+  array = groupBy(array, function (n) {
+    return n.pid;
+  });
+  let firstArray = array[firstPid];
+  transform(firstArray);
+  function transform(startList) {
+    if (startList)
+      for (let i = 0; i < startList.length; i++) {
+        array[startList[i][id]] && (startList[i][children] = array[startList[i][id]]);
+        transform(startList[i][children]);
+      }
+  }
+  return firstArray;
+}
+
+module.exports = smartArrayToTree;
---
+++ test/test.js
@@ -0,0 +1,46 @@
+'use strict';
+var smartArrayToTree = require('../index.js');
+var test = require('tape');
+
+test('totree test', function (t) {
+	let list=[
+	{
+		id:'1',
+		param1:'xx',
+		pid:null,
+	},
+			{
+		id:'2',
+		param1:'xx',
+		pid:null,
+	},
+			{
+		id:'3',
+		param1:'xx',
+		pid:null,
+	},
+			{
+		id:'11',
+		param1:'yy',
+		pid:'1',
+	},
+			{
+		id:'22',
+		param1:'yy',
+		pid:'2',
+	},
+			{
+		id:'33',
+		param1:'yy',
+		pid:'3',
+	},
+			]
+    let tree = smartArrayToTree({array:list});
+
+    t.equal(tree.length, 3);
+
+	t.end();
+});
+
+
+
