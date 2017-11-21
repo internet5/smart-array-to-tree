bd0774e562e7d02844f55d1abafde4a6e179f7af
internet5 <172039711@qq.com>
2017-11-21 18:06:43 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var smartArrayToTree = require('../index.js');
-var test = require('tape');
 var fetch = require('node-fetch');
 fetch('https://raw.githubusercontent.com/internet5/smart-array-to-tree/master/example/data.json').then(function(response) {
   return response.json();
