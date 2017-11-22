9faafe75fb102995007126244f208dfd8788b610
internet5 <172039711@qq.com>
2017-11-22 11:01:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict';
 
 var groupBy = require('lodash.groupby')
-const smartArrayToTree = ({ array, id = 'id', pid = 'pid', children = 'children', firstPid = null }) => {
+const smartArrayToTree = (array, options) => {
+  options = Object.assign({
+    id: 'id',
+    pid: 'pid',
+    children: 'children',
+	firstPid: null
+  }, options);
   let groupArray = groupBy(array, function (n) {
-    return n[pid];
+    return n[options.pid];
   });
-  let firstArray = groupArray[firstPid];
+  let firstArray = groupArray[options.firstPid];
   transform(firstArray);
   function transform(startList) {
     if (startList)
       for (let i = 0; i < startList.length; i++) {
-        groupArray[startList[i][id]] && (startList[i][children] = groupArray[startList[i][id]]);
-        transform(startList[i][children]);
+        groupArray[startList[i][options.id]] && (startList[i][options.children] = groupArray[startList[i][options.id]]);
+        transform(startList[i][options.children]);
       }
   }
   return firstArray;
